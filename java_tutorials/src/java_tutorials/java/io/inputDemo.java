9602bd1612e9e12c50768214ed1713c28805ec77
[Github <[Github>
2022-01-08 18:27:35 +0900

    removing all .git folder

---
+++ java_tutorials/src/java_tutorials/java/io/inputDemo.java
@@ -0,0 +1,12 @@
+package java_tutorials.java.io;
+
+public class inputDemo {
+
+	public static void main(String[] args) {
+		for(String e : args) {
+			System.out.println(e);
+		}
+		
+	}
+
+}
---
+++ java_tutorials/src/javatutorials/eclipse/Helloworld.java
@@ -0,0 +1,9 @@
+package javatutorials.eclipse;
+
+abstract class Helloworld {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
